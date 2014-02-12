d42d7e87260f7592aa6513b3352dd6e546f651f2
jimmynicol <james@fundly.com>
2014-02-12 12:12:26 -0800

    Added a precompile step the rails engine

--- lib/fundly/style/guide/engine.rb
+++ lib/fundly/style/guide/engine.rb
@@ -2,6 +2,9 @@ module Fundly
   module Style
     module Guide
       class Engine < ::Rails::Engine
+        initializer 'fundly-style-guide.assets.precompile' do |app|
+          app.config.assets.precompile << %w(fundly-style.css)
+        end
       end
     end
   end
