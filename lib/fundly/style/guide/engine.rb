9551aca5d6af3b8c20c7933677d79dd26fc74ade
John Chow <john@fundly.com>
2014-05-06 16:53:50 -0700

    Added :group => :all so it'd be loaded in all env

--- lib/fundly/style/guide/engine.rb
+++ lib/fundly/style/guide/engine.rb
@@ -2,8 +2,8 @@ module Fundly
   module Style
     module Guide
       class Engine < ::Rails::Engine
-        initializer 'fundly-style-guide.assets.precompile' do |app|
-          app.config.assets.precompile << %w(fundly-style.css)
+        initializer 'fundly-style-guide.assets.precompile', :group => :all do |app|
+          app.config.assets.precompile += %w(fundly-style.css)
         end
       end
     end
